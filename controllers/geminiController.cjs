2bf86a3c441ab9f72040d576258c7ee0d054c70e
Nick Olomek <nick.olomek@icloud.com>
2025-07-14 11:48:53 +0200

    changed to gemini-2.5-flash, because i got an overload error on the gemini-1.5-flash

--- controllers/geminiController.cjs
+++ controllers/geminiController.cjs
@@ -15,7 +15,7 @@ if (!apiKey) {
   process.exit(1);
 }
 const genAI = new GoogleGenerativeAI(apiKey);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
 async function logUnansweredQuestion(question) {
   try {
